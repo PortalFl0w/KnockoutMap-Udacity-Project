955cd296533a290fe33b18ec50852a947aff97d4
Matt L <mattlipski5543@gmail.com>
2018-09-08 11:23:53 +0100

    Sample Key File

---
+++ key_sample.js
@@ -0,0 +1,19 @@
+//
+//  PLEASE INPUT YOUR API KEY AND
+//  RENAME THIS FILE TO 'key.js'
+//
+
+const KEY = "YOUR_API_KEY"
+
+function createGoogleScript() {
+  let script = document.createElement("script");
+  let base = "https://maps.googleapis.com/maps/api/js";
+  let key = "?key=" + KEY;
+  let callback = "&callback=initMap"
+  let libs = "&libraries=places"
+  script.setAttribute('src', base + key + callback + libs);
+  script.setAttribute('async', '');
+  script.setAttribute('defer', '');
+  document.body.appendChild(script);
+}
+createGoogleScript();
