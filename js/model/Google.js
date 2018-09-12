5bc1354e597b1f129bf530131f569fe0380c74f9
Matt L <mattlipski5543@gmail.com>
2018-09-12 20:03:00 +0100

    Made suggested changes from Review 2

--- js/model/Google.js
+++ js/model/Google.js
@@ -11,7 +11,7 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: startingPos,
-    zoom: 17
+    zoom: 15
   });
 
   nearby(startingPos);
--- js/vm/ViewModel.js
+++ js/vm/ViewModel.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function ViewModel() {
 
   let self = this;
